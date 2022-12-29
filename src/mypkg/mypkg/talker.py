2e68365fce0f2fa6e28a282b6f83c833b7e4d976
Mark <masamasa.d127@gmail.com>
2022-12-29 15:28:19 +0900

    Add a command

--- src/mypkg/mypkg/talker.py
+++ src/mypkg/mypkg/talker.py
@@ -1,18 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10)
 n = 0
-
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()
+    msg.name = "上田隆一"
+    msg.age = n
     pub.publish(msg)
-    n +=1
+    n += 1
 
 node.create_timer(0.5, cb)
 rclpy.spin(node)
